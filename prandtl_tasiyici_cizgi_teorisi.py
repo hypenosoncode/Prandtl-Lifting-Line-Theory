b9e3ecb6b6e49cb1d679365b16b6281fc8f27521
İsmail Selçuk Çınar <74931027+hypenosoncode@users.noreply.github.com>
2022-04-12 02:31:56 +0300

    Update prandtl_tasiyici_cizgi_teorisi.py
    Converted to clean code. The code has been simplified and some bugs fixed.

--- prandtl_tasiyici_cizgi_teorisi.py
+++ prandtl_tasiyici_cizgi_teorisi.py
@@ -17,85 +17,82 @@ import math
 import numpy as np
 import matplotlib.pylab as plt
 
-# DEĞERLER
-N = 11 # İstasyon Sayısı
+# KANAT DEĞERLERİ - WING VALUES
+N = 11 # İstasyon Sayısı - Number of Stations
 S = 20.67 # Kanat Alanı [m^2] - Wing Area
 AR = 9.08 # Açıklık Oranı - Aspect Ratio
-TR = 0.80 # Taper Ratio
-alpha_hucum = 10.0 # Hücum Açısı [derece] - Twist Angle
-alpha_sonsuz = 5.98 # Taşıma Eğrisi Eğimi [derece] - Lift Curve Slope
+TR = 0.80 # Kök Oranı - Taper Ratio
+alpha_angle_of_attack = 10.0 # Hücum Açısı [derece] - Twist Angle
+alpha_infinity = 5.98 # Taşıma Eğrisi Eğimi [derece] - Lift Curve Slope
 alpha_zero_lift = -1.250 # Sıfır Taşıma Açısı [derece] - Zero Lift Angle of Attack
 
-# HESAPLAMALAR
-b = math.sqrt(AR * S) # Kanat Açıklığı (m)
-MAC = S / b # Ortalama Veter Uzunluğu (m)
-kok_chord = (1.5 * (1 + TR) * MAC) / (1 + TR + TR ** 2)  # Kök kanat[m] - root chord
-
-theta = np.linspace((math.pi / (2 * N)), (math.pi / 2), N) # Dizi oluşturuldu.
-
+# HESAPLAMALAR - CALCULATIONS
+b = math.sqrt(AR * S) # Kanat Açıklığı [m] - Wing Span
+MAC = S / b # Ortalama Veter Uzunluğu [m] - Mean Aerodynamic Chord
+root_chord = (1.5 * (1 + TR) * MAC) / (1 + TR + TR ** 2)  # Kök Kanat Uzunluğu [m] - Root Chord
+theta = np.linspace((math.pi / (2 * N)), (math.pi / 2), N) # π/2N den π/2 ye kadar olan bir dizi oluşturuldu. - A sequence from π/2N to π/2 is created.
 k = (b / 2) * np.cos(theta)
-c = kok_chord * (1 - (1 - TR) * np.cos(theta))
+c = root_chord * (1 - (1 - TR) * np.cos(theta))
 
-mu = alpha_sonsuz * c / (4 * b)
-mu_hesaplama_2 = mu * (np.array(alpha_hucum) - alpha_zero_lift)
-mu_hesaplama_2_rad = np.deg2rad(mu_hesaplama_2)
+# MÜ DEĞERİNİ HESAPLAMA ALANI - FIELD OF CALCULATION OF MU VALUE
+mu = alpha_infinity * c / (4 * b)
+mu_calculation_2 = mu * (np.array(alpha_angle_of_attack) - alpha_zero_lift)
+mu_calculation_2_rad = np.deg2rad(mu_calculation_2)
 
-# FORMÜL KISMI
-teta_grafik = []
+# FORMÜL KISMI - FORMULA PART
+theta_list = []
 for i in range(1, 2 * N + 1, 2):
-    teta_grafik_iter = np.sin(i * theta) * (1 + (mu * i) / (np.sin(list(theta))))
-    teta_grafik.append(teta_grafik_iter)
+    theta_calculation = np.sin(i * theta) * (1 + (mu * i) / (np.sin(list(theta))))
+    theta_list.append(theta_calculation)
 
-denklem = np.asarray(teta_grafik)
-denklem_transpoz = np.transpose(denklem)
-denklem_is_active = np.linalg.inv(denklem_transpoz)
+theta_equation = np.asarray(theta_list)
+theta_equation_transpoz = np.transpose(theta_equation)
+theta_equation_full = np.linalg.inv(theta_equation_transpoz) # Formül dizi olacak şekilde güzel bir şekilde düzenlendi. - The formula is nicely arranged to be an array.
 
-new_matrix = np.matmul(denklem_is_active, mu_hesaplama_2_rad)
-alan_deneme_sayilari = np.divide((4 * b), c)
+answer_list = np.matmul(theta_equation_full, mu_calculation_2_rad) # Hesaplamaya dahil edilebilmesi için yeni bir dizi oluşturuldu. - A new array has been created so that it can be included in the calculation.
+my_area_calculation = np.divide((4 * b), c) # Alanın değişimine göre hesaplanan değerler. - Values calculated based on the change of the field.
 
-iter0 = (np.sin((1) * theta)) * new_matrix[0] * alan_deneme_sayilari
-iter1 = (np.sin((3) * theta)) * new_matrix[1] * alan_deneme_sayilari
-iter2 = (np.sin((5) * theta)) * new_matrix[2] * alan_deneme_sayilari
-iter3 = (np.sin((7) * theta)) * new_matrix[3] * alan_deneme_sayilari
-iter4 = (np.sin((9) * theta)) * new_matrix[4] * alan_deneme_sayilari
-iter5 = (np.sin((11) * theta)) * new_matrix[5] * alan_deneme_sayilari
-iter6 = (np.sin((13) * theta)) * new_matrix[6] * alan_deneme_sayilari
-iter7 = (np.sin((15) * theta)) * new_matrix[7] * alan_deneme_sayilari
-iter8 = (np.sin((17) * theta)) * new_matrix[8] * alan_deneme_sayilari
-iter9 = (np.sin((19) * theta)) * new_matrix[9] * alan_deneme_sayilari
-iter10 = (np.sin((21) * theta)) * new_matrix[10] * alan_deneme_sayilari
+general_iter = 0
+for i in range(0, 10):
+    iter = (np.sin((2*i+1) * theta)) * answer_list[i] * my_area_calculation
+    general_iter = general_iter + iter
 
-# TAŞIMA KATSAYISI HESABI VE DEĞERLERİN OKUNABİLMESİ İÇİN MATRİSE ATANMASI
-CL_grafik = iter0 + iter1 + iter2 + iter3 + iter4 + iter5 + iter6 + iter7 + iter8 + iter9 + iter10 # Grafik 'e bastırılacak değerler
-CL_hesap = (math.pi * AR * new_matrix[0]) # Gerçek hesaplanan CL değeri
+# Hesaplamaya rho dahil edilmiştir. - Rho is included in the calculation.
+general_rho = 0 # Minimum sürükleme için rho sıfır kabul edilmelidir. - For minimum drag, rho should be assumed to be zero.
+for i in range(1, 10):
+    rho = (i+1 * (answer_list[i])**2 / (answer_list[0])**2)
+    general_rho = general_rho + rho
 
-rho = (2 * (new_matrix[1] / new_matrix[0])) + (3 * (new_matrix[2] / new_matrix[1])) + (4 * (new_matrix[3] / new_matrix[2])) + (5 * (new_matrix[4] / new_matrix[3])) + (6 * (new_matrix[5] / new_matrix[4])) + (7 * (new_matrix[6] / new_matrix[5]) + (8 * (new_matrix[7] / new_matrix[6]))) + (9 * (new_matrix[8] / new_matrix[7])) + (10 * (new_matrix[9] / new_matrix[8])) + (11 * (new_matrix[10] / new_matrix[9]))
+CL_calculation = (math.pi * AR * answer_list[0]) # Gerçek hesaplanan CL değeri - Actual calculated CL value
+CDi_induced_calculation = CL_calculation ** 2 / (math.pi * AR) * general_rho # Gerçek hesaplanan CL indüklenmiş değeri. - Actual calculated CL induced value.
+CL_induced_graph = general_iter ** 2 / (math.pi * AR) * general_rho # Grafik 'e bastılacak değerler. - Values to print to chart
 
-CL_induklenmis_grafik = CL_grafik ** 2 / (math.pi * AR) * rho # Grafik 'e bastılacak değerler
-CL_induklenmis_hesap = CL_hesap ** 2 / (math.pi * AR) * rho # Gerçek hesaplanan CL_indüklenmiş değeri
+# HESAPLANAN DEĞERLERİ BASTIRMA ALANI - SUPPRESSING THE CALCULATED VALUES AREA
+print("Tasima Katsayisi - Lift Coefficient (CL): ", CL_calculation)
+print("Induklenmis Surukleme Katsayisi - Induced Drag Coefficient (CDi): ", CDi_induced_calculation)
 
-# GRAFİK OLUŞTURMA ALANI
-CL1_dizi = np.append(0, CL_grafik)
-CL_induklenmis_dizi = np.append(0, CL_induklenmis_grafik)
-y_eksen = [b / 2, k[0], k[1], k[2], k[3], k[4], k[5], k[6], k[7], k[8], k[9], k[10]]
+# GRAFİK OLUŞTURMAK İÇİN DİZİ OLUŞTURMA ALANI - SERIES CREATION AREA TO CREATE A GRAPHIC    
+CL_array = np.append(0, general_iter)
+CDi_induced_array = np.append(0, CL_induced_graph)
 
-# HESAPLANAN DEĞERLERİ BASTIRMA ALANI
-print("Tasima Katsayisi: ", CL_hesap)
-print("Induklenmis Surukleme Katsayisi: ", CL_induklenmis_hesap)
+# X-EKSENİ UZUNLUĞU - X-AXIS LENGTH
+x_eksen = [b / 2]
+for i in range(0, N):
+    x_eksen.append(k[i])
 
-# GRAFİK BASTIRMA ALANI
-fig = plt.figure("AERODİNAMİK ÖDEVİ [12.GRUP] CL")
-plt.plot(y_eksen, CL1_dizi, marker="o")
-plt.title("NACA 65(2)-215")
-plt.xlabel("Kanat Yarı-Açıklık Konumu (m)")
-plt.ylabel("Taşıma Katsayısı")
+# GRAFİK BASTIRMA ALANI - GRAPHIC CREATING AREA
+fig1 = plt.figure("AERODYNAMICS HOMEWORK [12.GROUP] - CL")
+plt.plot(x_eksen, CL_array, marker=".")
+plt.title("NACA 65(2)-215 / CL")
+plt.xlabel("Kanat Yarı-Açıklık Konumu - Wing Half-Opening Position (m)")
+plt.ylabel("Taşıma Katsayısı - Lift Coefficient (CL)")
 plt.grid()
 plt.show()
 
-fig2 = plt.figure("AERODİNAMİK ÖDEVİ [12.GRUP] CL_induklenmis")
-plt.plot(y_eksen, CL_induklenmis_dizi, marker="o")
-plt.title("NACA 65(2)-215")
-plt.xlabel("Kanat Yarı-Açıklık Konumu (m)")
-plt.ylabel("İndüklenmiş Sürükleme Katsayısı")
+fig2 = plt.figure("AERODYNAMICS HOMEWORK [12.GROUP] - CDi")
+plt.plot(x_eksen, CDi_induced_array, marker=".")
+plt.title("NACA 65(2)-215 / CDi")
+plt.xlabel("Kanat Yarı-Açıklık Konumu - Wing Half-Opening Position (m)")
+plt.ylabel("İndüklenmiş Sürükleme Katsayısı - Induced Drag Coefficient (CDi)")
 plt.grid()
 plt.show()
